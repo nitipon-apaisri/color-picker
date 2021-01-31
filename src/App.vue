5b34c52a7a78a75ef097725a98cd00d8cb6e6621
David Lundholm <david.lundholm@zocom.se>
2021-01-31 21:45:29 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="{backgroundColor}">
+    <input type="text" v-model="text" @keydown.enter="backgroundColor = text">
+    <suggestion-list :match="text" @click="color => backgroundColor = color" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SuggestionList from '@/components/SuggestionList'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {SuggestionList},
+  data(){ return {
+    text: "",
+    backgroundColor: ""
+  }},
 }
 </script>
 
 <style lang="scss">
+body, html, #app{ margin: 0; height: 100%;}
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
 }
 </style>
---
+++ src/components/SuggestionList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li 
+      v-for="color in colors" 
+      :key="color"
+      @click="$emit('click', color)"
+    >{{color}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['match'],
+  data(){ return {
+    color: ['red','green','blue'] // Change this to a vuex getter that filters based on this.match
+  }},
+}
+</script>
+
+<style scoped lang="scss">
+ul{
+  position: absolute;
+}
+</style>
